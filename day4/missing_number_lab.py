8387fdf38922b6e5aacd636da37778db4f57a8e7
george-waf <georgewafulan@gmail.com>
2017-04-06 11:01:29 +0300

    day four projects
    work done on the fourth day of Andela boot camp

---
+++ day4/missing_number_lab.py
@@ -0,0 +1,35 @@
+def find_missing(list1, list2):
+    try:        
+        if len(list1) > len(list2):
+            return [x for x in list1 if x not in list2]
+        else:
+            return [x for x in list2 if x not in list1][0]
+    except IndexError:
+        return 0
+
+
+import unittest
+class MissingNumberTest(unittest.TestCase):
+    """docstring for MissingNumberTest"""
+
+    def test_empty_list(self):
+        self.assertEqual(0, find_missing([], []),
+                         msg='should return 0 for empty list')
+
+    def test_same_entries(self):
+        list1 = find_missing([2], [2])
+        list2 = find_missing([4], [4])
+        list3 = find_missing([7], [7])
+        self.assertListEqual([0, 0, 0],
+                             [list1, list2, list3],
+                             msg='should return 0 for lists with the same entries')
+
+    def test_missing_entries(self):
+        list1 = find_missing([1, 2], [1, 2, 5])
+        list2 = find_missing([4, 6, 8], [4, 6, 8, 10])
+        list3 = find_missing([5, 4, 7, 6, 11, 66], [5, 4, 1, 7, 6, 11, 66])
+        self.assertListEqual([5, 10, 1],
+                             [list1, list2, list3],
+                             msg='should return the missing number for lists with similar entries and a missing number')
+
+unittest.main(verbosity=2) 
